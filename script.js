49c1a1ba1b0300373aeb54e28759b75a0f45881d
Aly-gator <119552961+Kawaikimono@users.noreply.github.com>
2023-01-08 19:02:53 -0800

    Add HTML blocks

--- script.js
+++ script.js
@@ -19,5 +19,9 @@ $(function () {
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
+  // Add code to display the current date in the header of the page.
+  var today = dayjs();
+  $("#currentDay").text(today.format("dddd, MMMM D, YYYY"));
+  var hour = dayjx();
+  $console.log(today.format('HH'));
 });
